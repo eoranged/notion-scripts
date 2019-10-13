6008e55c17f27632e64076b135b65222005661f3
Vladimir Protasov <github@eoranged.com>
2019-10-13 16:18:34 +0300

    Add audit script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Random set of scripts to perform various opperations with Notion
+
+Uses [notion-py](https://github.com/jamalex/notion-py/) as a backend.
+
+# Scripts
+
+## audit.py
+
+Audit sharing settings for all pages in space.
---
+++ audit.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Get all shared pages from current space.
+"""
+from argparse import ArgumentParser
+from notion.client import NotionClient
+import itertools
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--token', required=True, help="token v2 for notion.so")
+    args = parser.parse_args()
+
+    client = NotionClient(token_v2=args.token)
+    space = client.current_space
+    store = client._store
+    user_id = client.current_user.id
+
+    # Fetch all pages, collections and space root
+    all_pages = [space] + [
+        client.get_block(page_id) for page_id in store._values['space'][client.current_space.id]['pages']]
+
+    blocks = list(all_pages)
+    while blocks:
+        page_ids = set((page.id for page in all_pages))
+        children_page_iter = itertools.chain.from_iterable(
+            (block.children for block in blocks if hasattr(block, 'children')))
+        children_iter = itertools.chain(
+            children_page_iter,
+            itertools.chain.from_iterable(
+                (row.get_all_properties().values() for row in block.get_rows())
+                for block in blocks
+                if getattr('block', 'type', None) == 'collection'
+            )
+        )
+        children = [
+            page for page in children_iter
+            if page.id not in page_ids and page.type in (
+                'page', 'collection',
+            )
+        ]
+        all_pages.extend(children)
+        blocks = children
+        print('Collected %s blocks, current children %s' % (len(all_pages), len(children)))
+
+    print('Total %s blocks' % len(all_pages))
+
+    for page in all_pages:
+        permissions = page.get('permissions')
+        if not permissions:
+            continue
+        permissions = [p for p in permissions if p.get('user_id') != user_id]
+        if permissions:
+            print('%s %s:' % (page.type, page.title))
+            for permission in permissions:
+                print('  > %s' % permission)
+
+
+if __name__ == '__main__':
+    main()
